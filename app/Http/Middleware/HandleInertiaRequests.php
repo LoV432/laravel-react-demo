8965218f923d3866eec7082eaf033a74b018107c
LoV432 <monib619@gmail.com>
2025-02-11 00:37:43 +0500

    pass venter_id prop with all response

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            'venter_id' => $request->cookie('venter_id'),
         ]);
     }
 }
