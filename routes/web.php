3b14a883a7d4ec4bdf2ea07787aac0dce9e9c774
LoV432 <monib619@gmail.com>
2025-02-11 00:37:16 +0500

    send venter and color data with message

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\MessageController;
+use App\Models\Message;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return Inertia::render('Home', [
-        'messagesData' => DB::table('messages')->orderBy('created_at', 'desc')->paginate(10),
+        'messagesData' => Message::with('venter')->with('color')->orderBy('created_at', 'desc')->paginate(10),
         // 'pageProps' => DB::select('select * from messages order by created_at desc limit 10'),
     ]);
 });
