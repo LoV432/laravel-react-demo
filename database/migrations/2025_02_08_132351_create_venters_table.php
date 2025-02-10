79a94854d05da16f8638d39e5771a51a66174ebd
LoV432 <monib619@gmail.com>
2025-02-11 00:36:53 +0500

    add users from json file if available

--- database/migrations/2025_02_08_132351_create_venters_table.php
+++ database/migrations/2025_02_08_132351_create_venters_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File;
+use App\Models\Venter;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 return new class extends Migration
 {
@@ -15,6 +18,24 @@ return new class extends Migration
             $table->id();
             $table->string('user_name');
         });
+        $path = base_path() . "/venters.json";
+        $ventersFile = '';
+        try {
+            $ventersFile = File::get($path);
+        } catch (FileNotFoundException $e) {
+            return;
+        }
+        $venters = json_decode($ventersFile);
+        $ventersArray = [];
+        foreach ($venters->{'users'} as $index => $venter) {
+            $ventersArray[] = ['user_name' => $venter->user_name, 'id' => $venter->id];
+            if ($index % 10000 == 0) {
+                // I am not sure where this ::insert is coming from but it works
+                // https://stackoverflow.com/questions/12702812/bulk-insertion-in-laravel-using-eloquent-orm
+                Venter::insert($ventersArray);
+                $ventersArray = [];
+            }
+        }
     }
 
     /**
