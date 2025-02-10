e3b0e9930b95aec17f3d107294335576b4f4990c
LoV432 <monib619@gmail.com>
2025-02-11 00:37:13 +0500

    update models

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
-    public $fillable = [
+    protected $fillable = [
         'message_text',
         'created_at',
         'venter_id',
--- app/Models/Venter.php
+++ app/Models/Venter.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venter extends Model
 {
-    public $fillable = [
+    protected $fillable = [
         'user_name',
     ];
 
