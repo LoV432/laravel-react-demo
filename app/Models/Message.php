898732176df39a4436658c3d19c95192453aee66
LoV432 <monib619@gmail.com>
2025-02-11 00:37:15 +0500

    add message > venter, color relation

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -16,4 +16,14 @@ class Message extends Model
     ];
 
     public $timestamps = false;
+
+    public function venter()
+    {
+        return $this->belongsTo(Venter::class);
+    }
+
+    public function color()
+    {
+        return $this->belongsTo(Color::class);
+    }
 }
