83a7e0ee7d8c069851748177f80213c15b1556a5
LoV432 <monib619@gmail.com>
2025-02-11 00:36:55 +0500

    add messages to db

--- database/migrations/2025_02_08_132502_create_messages_table.php
+++ database/migrations/2025_02_08_132502_create_messages_table.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Message;
 
 return new class extends Migration
 {
@@ -20,6 +22,22 @@ return new class extends Migration
             $table->uuid('uuid');
             $table->boolean('is_deleted')->default(false);
         });
+
+        $messagesFile = '';
+        try {
+            $messagesFile = File::get(base_path() . "/messages.json");
+        } catch (\Exception $e) {
+            return;
+        }
+        $messages = json_decode($messagesFile);
+        $messagesArray = [];
+        foreach ($messages->{'messages'} as $index => $message) {
+            $messagesArray[] = ['message_text' => $message->message_text, 'created_at' => $message->created_at, 'venter_id' => $message->user_id, 'color_id' => $message->color_id, 'uuid' => $message->uuid, 'is_deleted' => $message->is_deleted];
+            if ($index % 1000 == 0) {
+                Message::insert($messagesArray);
+                $messagesArray = [];
+            }
+        }
     }
 
     /**
